32c9555ccb45c7b3d9c88a20a454e2034825f638
endulsandovidas <endulsandovidas@gmail.com>
2026-05-20 19:25:48 -0500

    Se agrega nueva base de datos, agregamos una tabla espectacular y le cambiamos el nombre a otra tabla

--- init-db/goo.sql
+++ init-db/goo.sql
@@ -1,6 +1,13 @@
 CREATE SCHEMA IF NOT EXISTS goo;
 
-CREATE TABLE goo.accounts(
+CREATE TABLE goo.accounts (
+    id_acc UUID PRIMARY KEY,
+    email_acc TEXT NOT NULL UNIQUE,
+    name_acc TEXT NOT NULL,
+    picture_acc TEXT NOT NULL
+);
+
+CREATE TABLE goo.accounts_espectaculars (
     id_acc UUID PRIMARY KEY,
     email_acc TEXT NOT NULL UNIQUE,
     name_acc TEXT NOT NULL,
--- init-db/wac.sql
+++ init-db/wac.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS wac;
 
-CREATE TABLE wac.users(
+CREATE TABLE wac.all_users(
     id_use UUID PRIMARY KEY REFERENCES goo.accounts(id_acc),
     name_use TEXT
 );
